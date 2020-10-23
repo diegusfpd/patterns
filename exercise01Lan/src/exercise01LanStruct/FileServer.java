c6de394e73c3c82eb24014c44643df48c761623c
Guy Dierens <guy.dierens@sfpd.fgov.be>
2020-10-23 11:37:50 +0200

    correction in PrintServer

--- exercise01Lan/src/exercise01LanStruct/FileServer.java
+++ exercise01Lan/src/exercise01LanStruct/FileServer.java
@@ -1,10 +1,16 @@
 package exercise01LanStruct;
 
-public class FileServer extends Node{
+public class FileServer extends PacketHandler{
 
 	public FileServer(String address) {
 		super(address);
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void print(Packet packet) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- exercise01Lan/src/exercise01LanStruct/MQServer.java
+++ exercise01Lan/src/exercise01LanStruct/MQServer.java
@@ -1,10 +1,16 @@
 package exercise01LanStruct;
 
-public class MQServer extends Node{
+public class MQServer extends PacketHandler{
 
 	public MQServer(String address) {
 		super(address);
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void print(Packet packet) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
