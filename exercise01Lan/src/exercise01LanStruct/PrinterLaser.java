b72a0169d967162c15dbcc5238c603205c325ab0
Guy Dierens <guy.dierens@sfpd.fgov.be>
2020-10-23 09:43:53 +0200

    Test adapted

--- exercise01Lan/src/exercise01LanStruct/PrinterLaser.java
+++ exercise01Lan/src/exercise01LanStruct/PrinterLaser.java
@@ -5,6 +5,7 @@ public class PrinterLaser extends PrintServer implements PrintStrategy{
 	public PrinterLaser(String address) {
 		super(address);		
 	}
+	@Override
 	public void print(Packet packet) {
 		System.out.println(this.getAddress() + " " + packet.getContents() + " on Laser");
 	}
--- exercise01Lan/src/exercise01LanStruct/test/testClient.java
+++ exercise01Lan/src/exercise01LanStruct/test/testClient.java
@@ -3,6 +3,8 @@ package exercise01LanStruct.test;
 import exercise01LanStruct.Node;
 import exercise01LanStruct.Packet;
 import exercise01LanStruct.PrintServer;
+import exercise01LanStruct.PrinterInktjet;
+import exercise01LanStruct.PrinterLaser;
 import exercise01LanStruct.WorkStation;
 
 public class testClient {
@@ -11,9 +13,10 @@ public class testClient {
 		// TODO Auto-generated method stub
 		WorkStation w1 = new WorkStation("12");
 		Node n1 = new Node("n11");
-		PrintServer p1 = new PrintServer("printerabis");
+		//PrintServer p1 = new PrintServer("printerabis");
+		PrintServer p1 = new PrinterInktjet("printerabis");
 		Node n2 = new Node("n12");
-		PrintServer p2 = new PrintServer("printersfpd");
+		PrintServer p2 = new PrinterLaser("printersfpd");
 		Packet packet1 = new Packet("printersfpd","messagePrint");
 		
 		w1.setNextComponent(n1);
